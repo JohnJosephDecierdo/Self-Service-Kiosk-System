0594be0b10c0e1c3c9380d106ce30a78cec10f6e
JohnJosephDecierdo <johnjosephdecierdo5@gmail.com>
2026-04-21 02:42:18 +0800

    Fix qty decrease/increase in cart + fix analytics today date bug

--- obj/x86/Debug/net8.0-windows/OOP FINAL PROJECT.AssemblyInfo.cs
+++ obj/x86/Debug/net8.0-windows/OOP FINAL PROJECT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP FINAL PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d6ac260338404fd8bcff14e52ab67f942683ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ada2d8175403833e94af9fcd9b4cc118e1c5600")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP FINAL PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP FINAL PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
